35a88eeb6bbb9a0d934904b92bd938e64cb66f01
Thiri <thirinyeinaye23@gmail.com>
2019-11-01 09:15:55 +0630

    thiri

--- src/features/Home/Video/video.js
+++ src/features/Home/Video/video.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
 const video = (props) => {
+
     return (
-        <div>
-            I am video
+        <div
+        style={{
+          position: "relative",
+          paddingTop: 0,
+          height: "650px"
+        }} >
+            <iframe
+                style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                }}
+                src="https://www.youtube.com/embed/DX48mJjL7oU"
+                frameBorder="0"
+                allowTransparency="true"
+            />
+            {/* <button className="videoPoster" onClick={()=>handleClick()}></button> */}
+    
         </div>
     )
 }
